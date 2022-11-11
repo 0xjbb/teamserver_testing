22b2ba75f767dea558bc460417a4cee6f96e43ce
jb <jbork12@pm.me>
2022-11-11 22:43:28 +0000

    a

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import "fmt"
 
+wg := &sync.WaitGroup
+
+
+
 func main() {
 
 	test8080 := newListener("8080")
 	test8080.Spawn()
+
 	fmt.Println("blocking")
+
 	test8888 := newListener("8888")
 	test8888.Spawn()
 }
