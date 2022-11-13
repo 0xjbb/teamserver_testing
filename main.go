6f64f5f5e01ebb11bb6673f92767d0de65b9c14f
jb <jbork12@pm.me>
2022-11-13 16:52:16 +0000

    some code

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ wg := &sync.WaitGroup
 
 
 func main() {
-
+/*
 	test8080 := newListener("8080")
 	test8080.Spawn()
 
@@ -15,4 +15,20 @@ func main() {
 
 	test8888 := newListener("8888")
 	test8888.Spawn()
+*/
+
+server := http.NewServeMux()
+server.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+	io.WriteString(w, "This the main dashbaord area \n")
+
+})
+
+server.HandleFunc("/listener", func(w http.ResponseWriter, r *http.Request){
+	test8888 := newListener("8888")
+	test8888.Spawn()
+})
+
+http.ListenAndServe(":42069", testMux)
+
 }
+
